faacf44755cdaf3215362f8174a5a014cb5c2b42
sempeck <jarek.sepek@mediorama.pl>
2016-02-14 19:41:20 +0100

    Simple error handling

--- controllers/books-controller.js
+++ controllers/books-controller.js
@@ -3,19 +3,24 @@ var Book = require('../models/book');
 module.exports.create = function (req, res) {
   var book = new Book(req.body);
   book.save(function (err, result) {
-    console.log(req.body);
+    if (err)
+      res.send(err);
     res.json(result);
   });
 };
 
 module.exports.showOne = function (req, res) {
   Book.findById(req.params.id, function (err, result) {
+    if (err)
+      res.send(err);
     res.json(result);
   }
 )};
 
 module.exports.list = function (req, res) {
   Book.find({}, function (err, results) {
+    if (err)
+      res.send(err);
     res.json(results);
   });
 };
@@ -25,6 +30,8 @@ module.exports.edit = function (req, res) {
     result.author = req.body.author;
     result.title = req.body.title;
     result.save(function (err, result) {
+      if (err)
+        res.send(err);
       res.json(result);
     });
   });
--- models/Book.js
+++ models/Book.js
@@ -1,11 +1,3 @@
-// var mongoose = require('mongoose');
-
-// module.exports = mongoose.model('Book', {
-//   name: String,
-//   title: String
-// });
-
-
 var mongoose = require('mongoose');
 
 var BookSchema = new mongoose.Schema({
