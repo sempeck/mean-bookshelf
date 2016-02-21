cf87a1c1b44af9066f7bc24e0827efe32bf122aa
sempeck <jarek.sepek@mediorama.pl>
2016-02-21 11:56:22 +0100

    New Model

--- models/Book.js
+++ models/Book.js
@@ -1,10 +1,16 @@
 var mongoose = require('mongoose');
 
 var BookSchema = new mongoose.Schema({
-    author: String,
-    title: String
+    author: [ {name: String, surname: String} ],
+    title: String,
+    subtitle: String,
+    publisher: String,
+    isbn: String,
+    categories: [{category: String}],
+    tags: [{tag: String}],
+    image: String,
+    leased: Boolean
   }
 );
 
 module.exports = mongoose.model('Book', BookSchema);
-
